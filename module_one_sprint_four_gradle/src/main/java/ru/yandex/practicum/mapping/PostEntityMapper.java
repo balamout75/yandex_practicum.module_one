a213f4261addb37a8ddccf069c154a16469433ae
balamout75 <balamout75@gmail.com>
2025-11-08 17:26:31 +0800

    Начал тестировать

--- module_one_sprint_four_gradle/src/main/java/ru/yandex/practicum/mapping/PostEntityMapper.java
+++ module_one_sprint_four_gradle/src/main/java/ru/yandex/practicum/mapping/PostEntityMapper.java
@@ -19,4 +19,16 @@ public class PostEntityMapper  {
                 post.getLikesCount(),
                 post.getComments().size());
     }
+    public PostDto toDtoWithTitleForming(Post post) {
+        String title=post.getTitle();
+        if (title.length()>128) title=title.substring(0,128);
+        return new PostDto(post.getId(),
+                post.getTitle(),
+                title,
+                post.getTags().stream()
+                        .map(Tag::getTag)
+                        .toArray(String[]::new),
+                post.getLikesCount(),
+                post.getComments().size());
+    }
 }
